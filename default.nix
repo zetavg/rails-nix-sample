22b727b95ea7c9e5eca013a794e0d526caa7f4d5
zetavg <mail@zeta.vg>
2019-04-14 15:26:06 +0800

    update nix expressions

--- default.nix
+++ default.nix
@@ -1,46 +1,52 @@
 {
+  pkgs ? import (builtins.fetchGit {
+    url = "https://github.com/zetavg/nix-packages.git";
+    ref = "master";
+    rev = "70e88a3635bf940089d6c59ad56359067a2ef60c";
+  }) { },
+
+  ruby ? pkgs.ruby_2_5,
+
   name ? "sample-rails-app",
   railsEnv ? null,
   masterKey ? "b8085db5c6b1fe5cb794bc199c7a4313",
   developmentSecret ? "0d996176af46ffd1894d328d703f2b37",
   actionCableConfig ? null,
+  passengerForceMaxConcurrentRequestsPerProcess ? 0,
+
   packagePriority ? 100,
-  pkgs ? import ((import <nixpkgs> { }).fetchFromGitHub {
-    owner = "zetavg";
-    repo = "nix-packages";
-    rev = "a1078f6795ed3f5cd0ae29c86acbd6ca58107ee0";
-    sha256 = "0r1khcrbfdlpgv8w710mr1rnzdw89177r8hp3x86dfx5wp5gdnbd";
-  }) { },
-  ruby ? pkgs.ruby_2_5,
-  stdenv ? pkgs.stdenv,
-  coreutils ? pkgs.coreutils,
-  gnused ? pkgs.gnused,
-  rsync ? pkgs.rsync,
-  bundlerEnv ? pkgs.bundlerEnv,
-  bundler ? pkgs.bundler,
-  bundix ? pkgs.bundix,
+
   buildRailsApp ? pkgs.buildRailsApp,
+
   ...
 }:
 
 buildRailsApp {
-  src = ./.;
+  srcs = [
+    ./Gemfile
+    ./Gemfile.lock
+    ./config.ru
+    ./Rakefile
+    ./bin
+    ./config
+    ./app
+    ./public
+  ];
   gemfile = ./Gemfile;
   lockfile = ./Gemfile.lock;
   gemset = ./gemset.nix;
   inherit
+    ruby
     name
     railsEnv
     masterKey
     developmentSecret
     actionCableConfig
-    packagePriority
-    ruby
-    stdenv
-    coreutils
-    gnused
-    rsync
-    bundlerEnv
-    bundler
-    bundix;
+    packagePriority;
+  extraNginxPassengerConfig = ''
+    location /cable {
+      passenger_app_group_name ${name}-action-cable;
+      passenger_force_max_concurrent_requests_per_process ${toString passengerForceMaxConcurrentRequestsPerProcess};
+    }
+  '';
 }
